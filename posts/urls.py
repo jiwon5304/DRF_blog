41a2c7ed5948c668b6b01b00e6a75f2702402247
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-03-24 14:29:57 +0900

    generics

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,22 @@
 
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import (
+    PostView,
+    PostDetailView,
+    PostListView,
+    PostCreateView,
+    PostViewSet
+    )
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'posts', PostViewSet)
-
+    
 urlpatterns = [
+    path('post', PostView.as_view()),                               # 조회,생성: generics.ListCreateAPIView
+    path('post/<int:pk>', PostDetailView.as_view()),                # 개별조회, 수정, 삭제: APIView
+    path('list', PostListView.as_view()),                           # 조회: generics.ListAPIView
+    path('create', PostCreateView.as_view()),                       # 생성: generics.CreateAPIView
 ]
+
 urlpatterns += router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,121 @@
 from django.db.models import Q
 from rest_framework import status, viewsets, generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import NotFound
 from .models import Post
 from .serializers import PostSerializer
 
 
+### 조회,생성: generics.ListCreateAPIView
+class PostView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_queryset(self):
+
+        search_keyword = self.request.GET.get("keyword", None)
+
+        if search_keyword:
+            search_result = self.queryset.filter(
+                Q(title__icontains=search_keyword)
+                | Q(contents__icontains=search_keyword)
+            )
+            return search_result
+
+        else:
+            return self.queryset
+    
+    def get(self, request, *args, **kwargs):                # def list도 가능
+        page = self.paginate_queryset(self.get_queryset())
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):               # def create도 가능
+        serializer = self.serializer_class(
+            data=request.data,  
+            context={'user': request.user}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+### 개별조회,수정,삭제: APIView
+class PostDetailView(APIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]
+    
+    def get_object(self, pk=None):
+        try:
+            return self.queryset.get(pk=pk)
+        except Post.DoesNotExist:
+            raise NotFound('Not Found')
+    
+    def get(self, request, pk=None, *args, **kwargs):
+        post = self.get_object(pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+        
+    def put(self, request, pk=None, *args, **kwargs):
+        post = self.get_object(pk)
+        serializer = self.serializer_class(
+            post,
+            data=request.data,
+            context={'user': request.user},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None, *args, **kwargs):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+### 조회: generics.ListAPIView
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]
+    
+    # 1.페이지네이션 없을 때
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.get_queryset()
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
+    
+    # 2.페이지네이션 있을 때
+    # url: http://127.0.0.1:8000/api/list?page=2
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.get_queryset())
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+
+### 생성: generics.CreateAPIView
+class PostCreateView(generics.CreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(
+            data=request.data,  
+            context={'user': request.user}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      
+        
+### 생성,조회,개별조회,수정,삭제: viewset
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
